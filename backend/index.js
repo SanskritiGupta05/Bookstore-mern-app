effb890620f8980c9cf8b1d1df64e750e5b992d9
Sanskriti Gupta <sanskriti.gupta.005@gmail.com>
2024-02-07 00:25:46 +0530

    feat: update books using mongoose

--- backend/index.js
+++ backend/index.js
@@ -64,8 +64,6 @@ app.get('/books/:id', async (request, response) => {
     try {
 
         const {id} = request.params;
-
-
         const books = await Book.findById(id);
 
         return response.status(200).json(books);
@@ -75,8 +73,36 @@ app.get('/books/:id', async (request, response) => {
     }
 });
 
-// UPDATE a book
 
+// UPDATE a book
+app.put('/books/:id', async (request, response) =>{
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required fields: title,author,publishYear'
+            });
+        }
+
+        const {id} = request.params;
+        const result = await Book.findByIdAndUpdate(id, request.body);
+
+        if(!result){
+            return response.status(404).json({message:'Book not found'});
+        }
+        return response.status(200).send({message:'Book Updated Successfully'});
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+
+//mongoose
 mongoose
     .connect(mongoDBURL)
     .then(() => {
