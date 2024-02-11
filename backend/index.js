34243941564ca54cad984bd3b6c2ad03a6e08291
Sanskriti Gupta <sanskriti.gupta.005@gmail.com>
2024-02-11 12:27:43 +0530

    final commit

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,33 @@
-import  express, { request, response }  from "express";
+import  express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import booksRoute from "./routes/booksRoute.js"
+import cors from 'cors';
 
 const app = express(); 
 //middleware for parsing request body
 app.use(express.json());
+
+//Middleware for handing CORS POLICY
+//Option-1: Allow origins with Default of cors(*)
+app.use(cors());
+
+//Option - 2: Allow cutom origin - IT is also a BETTER way
+// app.use(
+//     cors({
+//         origin: 'http://localhost:3000',
+//         methods: ['GET','POST','PUT','DELETE'],
+//         allowedHeaders: ['Content-Type'],
+//     })
+// );
+
 //create a new route for / route
 // het is an http method generally used for getting resource from server
 app.get('/', (request,response) => {
     console.log(request)
     return response.status(234).send('Welcome!')
 });
+
 
 app.use('/books', booksRoute);
 
@@ -26,7 +42,7 @@ mongoose
             console.log(`App is listening to port: ${PORT}`);
         });
     })
-    .catch(() => {
+    .catch((error) => {
         console.log(error);
     });
     
---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import { Routes,Route } from 'react-router-dom'
+import Home from './pages/Home';
+import CreateBooks from './pages/CreateBooks';
+import ShowBooks from './pages/ShowBooks';
+import EditBooks from './pages/EditBooks';
+import DeleteBooks from './pages/DeleteBooks';
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/books/create' element={<CreateBooks />} />
+      <Route path='/books/details/:id' element={<ShowBooks />} />
+      <Route path='books/edit/:id' element={<EditBooks />} />
+      <Route path='/books/delete/:id' element={<DeleteBooks />} />
+    </Routes>
+  )
+}
+
+export default App
